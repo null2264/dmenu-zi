ebc587a5d3f022adacef0b43eaeccf9d5921fc80
ziro <palembani@gmail.com>
2020-07-19 16:41:51 +0700

    + Use twemoji instead of joypixels

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Iosevka:bold:size=9",
-	"JoyPixels:size=9"
+	"Twemoji:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
