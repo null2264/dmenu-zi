65c2735d923df6378b02f69b87f37a5fcc57a1ba
ziro <palembani@gmail.com>
2020-11-07 11:39:23 +0700

    + Added fuzzy match

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -14,6 +15,8 @@ static const char *colors[SchemeLast][2] = {
 	/*		  fg         bg       */
 	[SchemeNorm] = { "#f5f6fa", "#2f3640" },
 	[SchemeSel]  = { "#f5f6fa", "#37b0ff" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
