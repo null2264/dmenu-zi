3b5aa406580e6103bca2cc96921acdf35f437721
ziro <palembani@gmail.com>
2022-07-03 12:29:41 +0700

    + ziDark 2.0

--- config.def.h
+++ config.def.h
@@ -7,16 +7,22 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka:bold:size=9",
-	"Twemoji:size=9"
+	"Twemoji:size=9",
+	"Iosevka:bold:size=9"
 };
+
+static const char col_fg[] = "#e1f3ff";
+static const char col_bg[] = "#0e1418";
+static const char col_primary[] = "#37b0ff";
+static const char col_fgHigh[] = "#ffc978";
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*		  fg         bg       */
-	[SchemeNorm] = { "#f5f6fa", "#2f3640" },
-	[SchemeSel]  = { "#f5f6fa", "#37b0ff" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { col_fg, col_bg },
+	[SchemeSel]  = { col_fg, col_primary },
+	[SchemeSelHighlight] = { col_fgHigh, "#005577" },
+	[SchemeNormHighlight] = { col_fgHigh, "#222222" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
